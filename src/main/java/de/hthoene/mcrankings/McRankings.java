6e6b70d2eb6b1fa7dfddfdfeb4ea1247239ff174
hthoene <thoenehannes@gmail.com>
2023-05-20 16:42:31 +0200

    Updated pom.xml

--- src/main/java/de/hthoene/mcrankings/McRankings.java
+++ src/main/java/de/hthoene/mcrankings/McRankings.java
@@ -23,7 +23,7 @@ import java.util.logging.Level;
  * A full guide can be found <a href="https://mc-rankings.com/guide">here</a>
  *
  * @author Hannes Thoene
- * @version 1.0.3
+ * @version 1.0.4
  * @since 20.05.2023
  */
 public class McRankings {
